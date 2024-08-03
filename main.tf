83a868bbb67aa9df166d9e5d4d83305941100f7a
swaraj want <swarajwant@gmail.com>
2024-08-04 01:00:40 +0530

    added the secreutiy group code

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ resource "aws_instance" "myec2" {
   #ami = "ami-0e670eb768a5fc3d4"
   instance_type = var.instance_type["linux"]
   key_name      = var.ssh_key_name
+  security_groups = aws_security_group.allow_tls.id
 
   provisioner "local-exec" {
     #working_dir = "./ansible-first-run"
@@ -23,7 +24,21 @@ resource "aws_instance" "myec2" {
   }
 }
 
-#resource "aws_security_group" ""
-#}
+resource "aws_security_group" "allow_tls" {
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  #vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "webserver_sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = ["0.0.0.0/0"]
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
 
 
